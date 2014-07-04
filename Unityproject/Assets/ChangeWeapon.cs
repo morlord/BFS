491115ee14afa0e44ef1e61b98c56a30842bc3e7
morlord <rybindi@gmail.com>
2014-07-04 12:11:20 +0400

    меню
    переход в игру, корректировка стрельбы

---
+++ Unityproject/Assets/ChangeWeapon.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ChangeWeapon : MonoBehaviour {
+
+    private Button btn;
+	// Use this for initialization
+	void Start () {
+        btn = GetComponentInParent<Button>();
+        btn.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(4f, 66f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Unityproject/Assets/scripts/StartGame.cs
+++ Unityproject/Assets/scripts/StartGame.cs
@@ -1,23 +1,16 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityEngine.UI;
 
 public class StartGame : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-        var btn=GetComponent<Button>();
-        btn.onClick.AddListener(OnPointerClick);
-
+	void Start ()
+	{
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-
-        //GetComponent<Button>().transform.position = Camera.main.ViewportToScreenPoint(new Vector3(0.18f, 0.75f, 1));
-	}
-    
-    void OnPointerClick(Button btn)
+	    
+	public void LoadLevel()
     {
         Application.LoadLevel(1);
     }
