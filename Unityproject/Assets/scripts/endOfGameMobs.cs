cba57a27edf3b5ea8e2bbef2c3239e3fbe87ceef
morlord <rybindi@gmail.com>
2014-08-09 19:24:07 +0400

    endgame

---
+++ Unityproject/Assets/scripts/endOfGameMobs.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class endOfGameMobs : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D collider2)
+	{
+		if (collider2.gameObject.tag == "mob")
+		{
+			Application.LoadLevel(3);
+			DestroyObject(collider2.gameObject);
+			//k++;
+		}
+	}
+}
